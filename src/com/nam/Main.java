788940405d26bd38139d3eeefcbaa33a1ca44f5e
hp <namstudionsk@gmail.com>
2018-01-22 23:14:07 +0700

    Add some code

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println("wtf");
+
+        main2();
+    }
+
+    public static void main2()
+    {
+        System.out.println("X42");
     }
 }
